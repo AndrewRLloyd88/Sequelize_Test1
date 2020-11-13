52ba498157d1154938c980154d45316a8fb32088
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-11-12 17:19:07 -0800

    init commit

---
+++ routes/gigs.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/database");
+const Gig = require("../models/Gig");
+
+router.get("/", (req, res) =>
+  Gig.findAll()
+    .then((gigs) => {
+      console.log(gigs);
+      res.sendStatus(200);
+    })
+    .catch((error) => console.log("error: ", error))
+);
+
+module.exports = router;
